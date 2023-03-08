b1296a9f9949e712bf83fb0cfb4576d2cc442f7b
JUB_Yoush <yoush1337@gmail.com>
2023-03-07 21:34:38 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let time = document.getElementById("currentTime")
+updateTime();
+
+setInterval(()=>{
+updateTime();
+},1000)
+
+function updateTime(){
+    let dt = new Date().toLocaleString("en-US", {timeZone: "America/Toronto"})
+    year = dt.year
+    month = dt.month
+    day = dt.day
+    hour = dt.hour
+    minute = dt.minute
+    second = dt.second
+    time.textContent = dt
+
+}
