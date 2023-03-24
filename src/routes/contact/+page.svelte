99c5f1a48c19317be6530619c93c086187d520cf
JUB_Yoush <yoush1337@gmail.com>
2023-03-23 20:38:59 -0400

    svelte based blog

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,14 @@
+<h3>find me:</h3>
+<ul>
+  <li>
+    <a href="mailto:jayden.patrick.brooks@gmail.com"
+      >jayden.patrick.brooks@gmail.com</a
+    >
+  </li>
+  <li>yoush#9391 on discord</li>
+  <li><a href="https://www.linkedin.com/in/jaydenpb/">linkedin</a></li>
+</ul>
+<p>
+  note: Please understand I'll only respond to messages and proposals that align
+  with my interests and schedule. Thanks for reaching out!
+</p>
