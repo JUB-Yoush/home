c2fc2b2833b92a288d8dc343f67a0bdc2ad8416a
JUB_Yoush <yoush1337@gmail.com>
2023-03-23 21:26:20 -0400

    lets hope this works

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,14 @@
   src="https://media.tenor.com/sK6ZPrUy2hMAAAAC/izu-shaboten-animal-park-capybara.gif"
   alt=""
 />
-<div>web developer // game developer // lassonde CS 2025</div>
+<p>web developer // game developer // lassonde CS 2025</p>
 <h3>--links--</h3>
-<div class="linktree" />
+<div class="linktree">
+  <a href="http://github.com/JUB-Yoush" target="_blank">github </a> //
+  <a href="https://yoush.itch.io/" target="_blank">games </a> //
+  <a href="https://www.overleaf.com/read/wrcxdtrpqprx" target="_blank">resume</a
+  >
+  //
+  <a href="/contact.html">contact </a>//
+  <a href="/blog">blog(soon)</a>
+</div>
