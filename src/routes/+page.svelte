0564965af23e17e469fbb660220019bdcf4a67e6
JUB_Yoush <yoush1337@gmail.com>
2023-03-23 21:24:12 -0400

    we live, moving over old content5

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,12 +5,4 @@
 />
 <div>web developer // game developer // lassonde CS 2025</div>
 <h3>--links--</h3>
-<div class="linktree">
-  <a href="http://github.com/JUB-Yoush" target="_blank">github </a> //
-  <a href="https://yoush.itch.io/" target="_blank">games </a> //
-  <a href="https://www.overleaf.com/read/wrcxdtrpqprx" target="_blank">resume</a
-  >
-  //
-  <a href="/contact.html">contact </a>//
-  <a href="/blog">blog(soon)</a>
-</div>
+<div class="linktree" />
