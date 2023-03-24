ff04aba2acd87234b99c083526a94da7cbb34b4c
JUB_Yoush <yoush1337@gmail.com>
2023-03-23 21:17:30 -0400

    we live, moving over old content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<body>
+  <p id="currentTime" />
+  <h2>--jayden p brooks--</h2>
+  <img
+    src="https://media.tenor.com/sK6ZPrUy2hMAAAAC/izu-shaboten-animal-park-capybara.gif"
+    alt=""
+  />
+  <p>web developer // game developer // lassonde CS 2025</p>
+  <h3>--links--</h3>
+  <div class="linktree">
+    <a href="http://github.com/JUB-Yoush" target="_blank">github </a> //
+    <a href="https://yoush.itch.io/" target="_blank">games </a> //
+    <a href="https://www.overleaf.com/read/wrcxdtrpqprx" target="_blank"
+      >resume
+    </a>
+    //
+    <a href="/contact">contact </a>//
+    <a href="/blog"/>Blog (Sooner!!!)</a>
+  </div>
+</body>
