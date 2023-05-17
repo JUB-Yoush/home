0ec07a5c0fa303d01cf38624c283589ab7d4d91d
JUB_Yoush <yoush1337@gmail.com>
2023-05-16 21:39:25 -0400

    localstorage for flavors and gif changing works

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,73 @@
-//plain*
-// mint*
-// strawberry*
-//banana*
-// peanut
-// grape*
-//melon 
+
+const flavours = [
+
+    {"name":"plain",
+    "bg_colour":"#e5d6cc",
+    "text_colour":"#171717",
+    "link_color":"#878f8f"},
+
+    {"name":"mint",
+    "bg_colour":"#e6f7ff",
+    "text_colour":"#3b3350",
+    "link_color":"#f7e8a9"},
+    
+
+    {"name":"strawberry",
+    "bg_colour":"#ffcde6",
+    "text_colour":"#422549",
+    "link_color":"#f7e8c8"},
+
+    {"name":"banana",
+    "bg_colour":"#fffeb8",
+    "text_colour":"#493351",
+    "link_color":"#f7f8d0"},
+
+
+    {"name":"peanut",
+    "bg_colour":"#fddebc",
+    "text_colour":"#411e2c",
+    "link_color":"#f7e8a9"},
+
+
+    {"name":"grape",
+    "bg_colour":"#e7d4f6",
+    "text_colour":"#2c0d2c",
+    "link_color":"#878f8f"},
+
+    {"name":"melon",
+    "bg_colour":"#dff5de",
+    "text_colour":"#0d142c",
+    "link_color":"#878f8f"},
+]
+
+if (!localStorage.getItem("flavour")){
+    populate_storage()
+    render_flavor(0)
+}else{
+    render_flavor(localStorage.getItem("flavour"))
+}
+
+function populate_storage(){
+    localStorage.setItem("flavour",0)
+}
+
+
+
+function change_flavor(){
+    var current_flavor = localStorage.getItem("flavour")
+    current_flavor++
+    if (current_flavor == flavours.length)
+    {current_flavor = 0}
+    render_flavor(current_flavor)
+}
+
+function render_flavor(current_flavor){
+    console.log('changing to',flavours[current_flavor])
+    const body = document.body
+    const siteFlavour = document.querySelector('.site-flavor > a')
+    siteFlavour.textContent = flavours[current_flavor].name
+    body.style.backgroundColor = flavours[current_flavor].bg_colour
+    body.style.color = flavours[current_flavor].text_colour
+
+    localStorage.setItem("flavour",current_flavor)
+}
