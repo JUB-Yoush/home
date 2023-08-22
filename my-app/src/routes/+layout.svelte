2f2bfe7cc8887df63dba7c81b20d8ac0b1659e7d
JUB_Yoush <Yoush1337@gmail.com>
2023-08-22 12:00:31 -0400

    svk port commit

---
+++ my-app/src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+
+<link href='https://fonts.googleapis.com/css?family=Ubuntu Mono' rel='stylesheet'>
+<script>
+  import "../css/tachyons.css";
+  import "../css/global.css";
+</script>
+<slot/>
---
+++ my-app/src/routes/+page.svelte
@@ -0,0 +1,2 @@
+<h1 class="ma5">Jayden Patrick Brooks</h1>
+<p>big new sveltekit port</p>
