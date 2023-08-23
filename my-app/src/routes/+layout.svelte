c9371b5bf5e868e676562576e1b5b4825a407f05
JUB_Yoush <yoush1337@gmail.com>
2023-08-23 19:28:39 -0400

    fixing

--- my-app/src/routes/+layout.svelte
+++ my-app/src/routes/+layout.svelte
@@ -1,73 +1,106 @@
-<link href='https://fonts.googleapis.com/css?family=Spline Sans Mono' rel='stylesheet'>
-<script>
-  import "../css/tachyons.css"
-  import "../css/global.css";
-  //FLAVOUR LOGIC
-  const flavors = [
+<script ssr={false}>
+	import '../css/tachyons.css';
+	import '../css/global.css';
+    import {onMount } from 'svelte'
+    import {session} from '$app/stores'
+	//FLAVOUR LOGIC
 
-    {"name":"plain",
-    "bg_color":"#e5d6cc",
-    "text_color":"#171717",
-    "link_color":"#878f8f"},
+	const flavors = [
+		{ name: 'plain', bg_color: '#e5d6cc', text_color: '#171717', link_color: '#878f8f' },
 
-    {"name":"mint",
-    "bg_color":"#e6f7ff",
-    "text_color":"#3b3350",
-    "link_color":"#66b7b0"},
-    
+		{ name: 'mint', bg_color: '#e6f7ff', text_color: '#3b3350', link_color: '#66b7b0' },
 
-    {"name":"strawberry",
-    "bg_color":"#ffcde6",
-    "text_color":"#422549",
-    "link_color":"#f179a0"},
+		{ name: 'strawberry', bg_color: '#ffcde6', text_color: '#422549', link_color: '#f179a0' },
 
-    {"name":"banana",
-    "bg_color":"#fffeb8",
-    "text_color":"#493351",
-    "link_color":"#e9c46a"},
+		{ name: 'banana', bg_color: '#fffeb8', text_color: '#493351', link_color: '#e9c46a' },
 
+		{ name: 'peanut', bg_color: '#fddebc', text_color: '#411e2c', link_color: '#a97766' },
 
-    {"name":"peanut",
-    "bg_color":"#fddebc",
-    "text_color":"#411e2c",
-    "link_color":"#a97766"},
+		{ name: 'grape', bg_color: '#e7d4f6', text_color: '#2c0d2c', link_color: '#878f8f' },
 
-
-    {"name":"grape",
-    "bg_color":"#e7d4f6",
-    "text_color":"#2c0d2c",
-    "link_color":"#878f8f"},
-
-    {"name":"melon",
-    "bg_color":"#dff5de",
-    "text_color":"#0d142c",
-    "link_color":"#878f8f"},
-]
-
-</script>
-<body class=" ma2 pt7-ns  flex flex-column justify-center items-center">
+		{ name: 'melon', bg_color: '#dff5de', text_color: '#0d142c', link_color: '#878f8f' }
+	];
    
-    
-    <div class="mr-auto ml-auto flex flex-column flex-row-ns ">
-        <div class="mt5 tr flex flex-row flex-wrap flex-column-ns">
-            <div class="mt2 mr3"><a class="link" href="index">home</a></div>
-            <div class="mt2 mr3"><a class="link" href="about">about</a></div>
-            <div class="mt2 mr3"><a class="link" href="resume">hire me 😳</a></div>
-            <div class="mt2 mr3"><a class="link" href="projects">projects</a></div>
-            <div class="mt2 mr3"><a class="link" href="posts">blog</a></div> 
-            <div class="site-flavor mt2 mr3"><a class="link" href="javascript:;" onclick="change_flavor()">plain</a></div>
-        </div>
-        <div class="mw7">
-            <slot/>
-        </div>
-    </div>
-    <div class="border">
-        <div>----------------------------</div>
-    </div>
-    <div class="links">
-        <a class="link" href="mailto:jayden.patrick.brooks@gmail.com"target="_blank">email</a>
-        <a class="link" href="https://www.linkedin.com/in/jaydenpb/"target="_blank">linkedin</a>
-        <a class="link" href="https://github.com/JUB-Yoush"target="_blank">github</a>
-        <a class="link" href="https://yoush.itch.io/"target="_blank">itch.io</a>
-    </div>
+    $session.subscribe(($session) => {
+        if (typeof $session.flavor !== 'undefined') {
+            sessionStorage.setItem('flavor_index',$session.flavor)
+        }
+    });
+
+    onMount(()=>{
+        if (typeof window !== 'undefined'){
+            if (!sessionStorage.getItem('flavor_index')){
+                populate_storage();
+                $session.flavor = flavors[0]
+            }
+            else{
+                $session.flavor = flavors[sessionStorage.getItem('flavor_index')];
+            }
+        }
+            function populate_storage(){
+                sessionStorage.setItem("flavor_index",0)
+        }
+    }
+    );
+
+function change_flavor(){
+    let flavor_index = sessionStorage.getItem("flavor_index")
+    flavor_index++
+    if (flavor_index == flavors.length)
+    {flavor_index = 0}
+    let current_flavor = flavors[flavor_index]
+    sessionStorage.setItem("flavor_index",flavor_index)
+    render_flavor(current_flavor)
+}
+
+function render_flavor(current_flavor){
+    const body = document.body
+    const siteflavor = document.querySelector('.site-flavor > a')
+
+    let links = document.getElementsByTagName("a");
+    for(var i=0;i<links.length;i++)
+    {   links[i].style.color = current_flavor.text_color;  
+        links[i].addEventListener('mouseover',function(){
+            this.style.color = current_flavor.link_color
+        });
+        links[i].addEventListener('mouseout',function(){
+            this.style.color = current_flavor.text_color
+        });}   
+    siteflavor.textContent = current_flavor.name
+
+    body.style.backgroundColor = current_flavor.bg_color
+    body.style.color = current_flavor.text_color
+}
+    //setup storage for first time
+</script>
+
+<link href="https://fonts.googleapis.com/css?family=Spline Sans Mono" rel="stylesheet" />
+<div class="border pt4-ns">
+    <div>----------------------------</div>
+</div>
+<body class=" ma2  flex flex-column justify-center items-center">
+	<div class="mr-auto ml-auto flex flex-column flex-row-ns">
+		<div class="mt3 tr flex flex-row flex-wrap flex-column-ns">
+			<div class="mt2 mr3"><a class="link" href="index">home</a></div>
+			<div class="mt2 mr3"><a class="link" href="about">about</a></div>
+			<div class="mt2 mr3"><a class="link" href="resume">hire me 😳</a></div>
+			<div class="mt2 mr3"><a class="link" href="projects">projects</a></div>
+			<div class="mt2 mr3"><a class="link" href="posts">blog</a></div>
+			<div class="site-flavor mt2 mr3">
+				<a class="link" href="javascript:;" onclick="change_flavor()">{current_flavor.name}</a>
+			</div>
+		</div>
+		<div class="mw6"style="max-width:40rem">
+			<slot />
+		</div>
+	</div>
+	<div class="border">
+		<div>----------------------------</div>
+	</div>
+	<div class="links">
+		<a class="link" href="mailto:jayden.patrick.brooks@gmail.com" target="_blank">email</a>
+		<a class="link" href="https://www.linkedin.com/in/jaydenpb/" target="_blank">linkedin</a>
+		<a class="link" href="https://github.com/JUB-Yoush" target="_blank">github</a>
+		<a class="link" href="https://yoush.itch.io/" target="_blank">itch.io</a>
+	</div>
 </body>
--- my-app/src/routes/+page.md
+++ my-app/src/routes/+page.md
@@ -1,6 +1,34 @@
 <script>
-    //import Base from 
-</script>
-<h1 class="">Jayden Patrick Brooks makrdown</h1>
+ 
+  const home_gifs = ["https://media.tenor.com/sK6ZPrUy2hMAAAAC/izu-shaboten-animal-park-capybara.gif","https://media.tenor.com/O45wy7ceQVEAAAAd/daigo-parry-fight.gif","https://media.tenor.com/qHGyE8XvwzUAAAAC/mother3.gif","https://media.tenor.com/U5ruYqXgPWIAAAAC/luffy-baron-omatsuri.gif"]
+const home_gif_quotes = ["capybara",'"Lets go Justin!!"',"Check out the site's color themes","I love one piece."]
+let gif_number = Math.floor(Math.random() * home_gifs.length)
+console.log(gif_number)
 
-<p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
+
+function swap_gif(){
+    gif_number++
+    if (gif_number == home_gifs.length){
+        gif_number = 0
+    }
+}
+
+</script>
+<div class = "home-content flex flex-column ">
+
+
+# Jaydenpb
+<img class="home-gif mw6 w-50 h-auto" style="" src={home_gifs[gif_number]} alt="">
+
+<a class="gif-changer" href="javascript:;" onclick="swap_gif()">{home_gif_quotes[gif_number]}</a>
+
+Ayo 
+
+I'm Jayden, a computer science student at the Lassonde school of Engineering (which is at York University) 
+If you're a recruiter who's found this link though my linkedin then you should definitely consider [hiring](/hire) me, Looking for Winter 2024 and Summer 2024 positions
+
+If you found this link through my discord then feel free to learn more [about me](/about) about me, check out what I'm [up to](/projects) or what I'm [thinking about](/posts).
+
+regardless, thanks for checking out my site, feel free to reach out!
+
+</div>
