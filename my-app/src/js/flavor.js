6b7f6e5bc3a783ebcf7e102a8b1289c2299575c1
JUB_Yoush <yoush1337@gmail.com>
2023-08-24 21:16:42 -0400

    tried store

---
+++ my-app/src/js/flavor.js
@@ -0,0 +1,29 @@
+import { browser } from "$app/environment";
+import { writable } from "svelte/store";
+
+   export const flavorRef = [
+		{ name: 'plain', bg_color: '#e5d6cc', text_color: '#171717', link_color: '#878f8f' },
+
+		{ name: 'mint', bg_color: '#e6f7ff', text_color: '#3b3350', link_color: '#66b7b0' },
+
+		{ name: 'strawberry', bg_color: '#ffcde6', text_color: '#422549', link_color: '#f179a0' },
+
+		{ name: 'banana', bg_color: '#fffeb8', text_color: '#493351', link_color: '#e9c46a' },
+
+		{ name: 'peanut', bg_color: '#fddebc', text_color: '#411e2c', link_color: '#a97766' },
+
+		{ name: 'grape', bg_color: '#e7d4f6', text_color: '#2c0d2c', link_color: '#878f8f' },
+
+		{ name: 'melon', bg_color: '#dff5de', text_color: '#0d142c', link_color: '#878f8f' }
+	];
+
+    const default_value = 0
+    const initial_value = browser ? window.localStorage.getItem("flavor") ?? default_value : default_value;
+
+    export const flavorStore = writable(initial_value);
+
+    flavorStore.subscribe((value) => {
+        if (browser){
+            window.localStorage.setItem('flavor',value)
+        }
+    });
--- my-app/src/routes/+layout.svelte
+++ my-app/src/routes/+layout.svelte
@@ -1,71 +1,25 @@
 <script>
 	import '../css/tachyons.css';
 	import '../css/global.css';
+    import {flavorStore} from '../js/flavor'
+    import {flavorRef} from '../js/flavor'
     import {onMount } from 'svelte'
     import {writable} from 'svelte/store';
 	//FLAVOUR LOGIC
 
-	const flavors = [
-		{ name: 'plain', bg_color: '#e5d6cc', text_color: '#171717', link_color: '#878f8f' },
 
-		{ name: 'mint', bg_color: '#e6f7ff', text_color: '#3b3350', link_color: '#66b7b0' },
+    $: current_flavor_id = $flavorStore
+    $: update_flavor(current_flavor_id)
+    console.log(flavorRef)
 
-		{ name: 'strawberry', bg_color: '#ffcde6', text_color: '#422549', link_color: '#f179a0' },
-
-		{ name: 'banana', bg_color: '#fffeb8', text_color: '#493351', link_color: '#e9c46a' },
-
-		{ name: 'peanut', bg_color: '#fddebc', text_color: '#411e2c', link_color: '#a97766' },
-
-		{ name: 'grape', bg_color: '#e7d4f6', text_color: '#2c0d2c', link_color: '#878f8f' },
-
-		{ name: 'melon', bg_color: '#dff5de', text_color: '#0d142c', link_color: '#878f8f' }
-	];
-
-    const current_flavor = writable({});
-
-    onMount(()=> {
-        if (typeof window !== "undefined"){
-            if (!sessionStorage.getItem('flavor_index')){
-                populate_storage();
-                change_flavor(0);
-            }else{
-                change_flavor(parseInt(sessionStorage.getItem("flavor_index")));
-            }
+    function update_flavor(current_flavor_id){
+        if (current_flavor_id > flavorRef.length){
+            current_flavor_id == 0
+            flavorStore.set(0)
         }
-    })
-    function populate_storage(){
-        sessionStorage.setItem("flavor_index",0)
+        console.log(current_flavor_id)
     }
-
-    function change_flavor(flavor_index){
-        flavor_index++
-        if (flavor_index == flavors.length)
-        {flavor_index = 0}
-        const selected_flavor = flavors[flavor_index]
-        let current_flavor = flavors[flavor_index]
-        sessionStorage.setItem("flavor_index",flavor_index)
-        render_flavor(current_flavor)
-        current_flavor.setItem(selected_flavor)
-    }
-
-    function render_flavor(current_flavor){
-        const body = document.body
-        const siteflavor = document.querySelector('.site-flavor > a')
-
-        let links = document.getElementsByTagName("a");
-        for(var i=0;i<links.length;i++)
-        {   links[i].style.color = current_flavor.text_color;  
-            links[i].addEventListener('mouseover',function(){
-                this.style.color = current_flavor.link_color
-            });
-            links[i].addEventListener('mouseout',function(){
-                this.style.color = current_flavor.text_color
-            });}   
-        siteflavor.textContent = current_flavor.name
-
-        body.style.backgroundColor = current_flavor.bg_color
-        body.style.color = current_flavor.text_color
-    }
+    
         //setup storage for first time
 </script>
 
@@ -82,7 +36,7 @@
 			<div class="mt2 mr3"><a class="link" href="projects">projects</a></div>
 			<div class="mt2 mr3"><a class="link" href="posts">blog</a></div>
 			<div class="site-flavor mt2 mr3">
-				<a class="link" href="javascript:;" on:click={change_flavor(2)}>{current_flavor.name}</a>
+				<a class="link" href="javascript:;" on:click={()=> update_flavor(current_flavor_id++)}>placehold</a>
 			</div>
 		</div>
 		<div class="mw6"style="max-width:40rem">
