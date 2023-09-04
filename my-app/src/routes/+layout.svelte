61f34e799b901c095bf179a4c33ca18e73bbf739
JUB_Yoush <yoush1337@gmail.com>
2023-09-04 13:24:49 -0400

    amongus

--- my-app/src/routes/+layout.svelte
+++ my-app/src/routes/+layout.svelte
@@ -5,47 +5,65 @@
     import {flavorRef} from '../js/flavor'
     import {onMount } from 'svelte'
     import {writable} from 'svelte/store';
+	import {browser} from '$app/environment';
 	//FLAVOUR LOGIC
 	let local_flavor_index
+	let anchor_array = []
+	let read = false
 	curr_flavor_index.subscribe((index)=>local_flavor_index = index)
 	
-	function change_flavour(){
-		if (local_flavor_index > flavorRef.length){
-            local_flavor_index = 0
-		}else{
-			local_flavor_index+=1
-		}
-		curr_flavor_index.set(local_flavor_index)
-		console.log(curr_flavor_index,local_flavor_index)
-	}
+	
 
 	let savestore = false
   $: if (savestore && $curr_flavor_index) {
     window.sessionStorage.setItem("store", JSON.stringify($curr_flavor_index))
   }
 
-    onMount(async () => {
-    let ses = window.sessionStorage.getItem("store")
-      if (ses) {
-        console.log("sob-- ~ loading ses", ses)
-        $curr_flavor_index = JSON.parse(ses)
-      }
-    savestore = true})
-
-	/*
+    onMount(() => {
+		let ses = window.sessionStorage.getItem("store")
+		if (ses) {
+			console.log("sob-- ~ loading ses", ses)
+			$curr_flavor_index = JSON.parse(ses)
 		}
-		} let current_flavor_id = flavorStore
-    $: update_flavor(current_flavor_id)
-    console.log(flavorRef)
+		savestore = true
+		render_flavor(local_flavor_index)
+	})
 
-    function update_flavor(current_flavor_id){
-        if (current_flavor_id > flavorRef.length){
-            current_flavor_id == 0
-            flavorStore.set("amog")
-        }
-        console.log(current_flavor_id)
-    }
-	*/
+
+	function change_flavour(){
+		local_flavor_index += 1
+		if (local_flavor_index > flavorRef.length -1){
+            local_flavor_index = 0
+		}
+		//curr_flavor_index.set(local_flavor_index)
+		window.sessionStorage.setItem("store",local_flavor_index)
+		console.log(curr_flavor_index,local_flavor_index,typeof(local_flavor_index))
+		render_flavor(local_flavor_index)
+	}
+
+	let bodyStyles = {};
+	let linkStyles = {};
+	
+	function render_flavor(flavor_index){
+		if (browser) {
+			
+			const current_flavor = flavorRef[flavor_index]
+			const body = document.body
+
+			let links = document.getElementsByTagName("a");
+			for(var i=0;i<links.length;i++)
+			{   links[i].style.color = current_flavor.text_color;  
+				links[i].addEventListener('mouseover',function(){
+					this.style.color = current_flavor.link_color
+				});
+				links[i].addEventListener('mouseout',function(){
+					this.style.color = current_flavor.text_color
+				});}   
+
+			body.style.backgroundColor = current_flavor.bg_color
+			body.style.color = current_flavor.text_color
+		}
+	}
     
         //setup storage for first time
 </script>
@@ -57,13 +75,13 @@
 <body class=" ma2  flex flex-column justify-center items-center">
 	<div class="mr-auto ml-auto flex flex-column flex-row-ns">
 		<div class="mt3 tr flex flex-row flex-wrap flex-column-ns">
-			<div class="mt2 mr3"><a class="link" href="index">home</a></div>
-			<div class="mt2 mr3"><a class="link" href="about">about</a></div>
-			<div class="mt2 mr3"><a class="link" href="resume">hire me 😳</a></div>
-			<div class="mt2 mr3"><a class="link" href="projects">projects</a></div>
-			<div class="mt2 mr3"><a class="link" href="posts">blog</a></div>
+			<div class="mt2 mr3"><a class="" href="index">home</a></div>
+			<div class="mt2 mr3"><a class="" href="about">about</a></div>
+			<div class="mt2 mr3"><a class="" href="resume">hire me 😳</a></div>
+			<div class="mt2 mr3"><a class="" href="projects">projects</a></div>
+			<div class="mt2 mr3"><a class="" href="posts">blog</a></div>
 			<div class="site-flavor mt2 mr3">
-				<a class="link" href="javascript:;" on:click={change_flavour}>placehold</a>
+				<a class="" href="javascript:;" on:click={change_flavour}>{flavorRef[local_flavor_index].name}</a>
 			</div>
 		</div>
 		<div class="mw6"style="max-width:40rem">
@@ -74,9 +92,9 @@
 		<div>----------------------------</div>
 	</div>
 	<div class="links">
-		<a class="link" href="mailto:jayden.patrick.brooks@gmail.com" target="_blank">email</a>
-		<a class="link" href="https://www.linkedin.com/in/jaydenpb/" target="_blank">linkedin</a>
-		<a class="link" href="https://github.com/JUB-Yoush" target="_blank">github</a>
-		<a class="link" href="https://yoush.itch.io/" target="_blank">itch.io</a>
+		<a class="" href="mailto:jayden.patrick.brooks@gmail.com" target="_blank">email</a>
+		<a class="" href="https://www.linkedin.com/in/jaydenpb/" target="_blank">linkedin</a>
+		<a class="" href="https://github.com/JUB-Yoush" target="_blank">github</a>
+		<a class="" href="https://yoush.itch.io/" target="_blank">itch.io</a>
 	</div>
 </body>
